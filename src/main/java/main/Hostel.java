67be11f241310e8dc584eac19c7e9cc3a9a861ff
Hello <Hello@DESKTOP-MNLDJF6>
2019-05-14 21:01:47 +0530

    successfully completed

--- src/main/java/main/Hostel.java
+++ src/main/java/main/Hostel.java
@@ -39,18 +39,29 @@ public class Hostel{
 	               while ((line = br.readLine()) != null) {
 		               String[] splited = line.split("\\s+");
 		               String checkName = splited[0];
+		               if(splited[0].equals(name))
+		               {
+		            	   return true;
+		               }
 		               //write your code here !!!
-//		               compare check name with name and return true if present and false if not
+        // compare check name with name and return true if present and false if not
 	               }
 	               
 	               
 	            }catch(Exception e){
 	                System.out.println(e);
 	            }
-			return true;
+			return false;
 	   }
        public static void allotHostel(){
     	   //write your code here!!!
+    	   try
+    	   {
+    		   writedata();
+    	   }catch(IOException e)
+    	   {
+    		   System.out.println(e);
+    	   }
     	   
        }
 
@@ -76,6 +87,7 @@ public class Hostel{
     	   boolean chk = true;
     	   
     	   //write your code here
+    	   chk=readData(name);
     	   
     	   return chk;
         }
